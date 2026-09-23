494b53ec81e6fb060940d4573c8476723a0afc51
Grezia <a01713876@tec.mx>
2026-09-22 18:17:18 -0600

    clean up of files to avoid issues

--- app/src/main/java/com/example/adaxintegra/presentation/views/screens/cases/CaseProgressScreen.kt
+++ app/src/main/java/com/example/adaxintegra/presentation/views/screens/cases/CaseProgressScreen.kt
@@ -1,8 +1,6 @@
 package com.example.adaxintegra.presentation.views.screens.cases
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,13 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.adaxintegra.presentation.navigation.BottomNavBar
-import com.example.adaxintegra.presentation.navigation.BottomNavBarItem
 import com.example.adaxintegra.presentation.viewmodel.CaseProgressViewModel
 import com.example.adaxintegra.presentation.views.designsystem.organisms.AppHeader
 import com.example.adaxintegra.presentation.views.designsystem.organisms.timeline.CaseProgressTimeline
